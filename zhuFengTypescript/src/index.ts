afbebb900514cc52e48279d72e7167e0e1725fbb
张永霖 <2678464408@qq.com>
2025-09-24 22:26:25 +0800

    feat: 添加 Person 类及实例创建函数

--- zhuFengTypescript/src/index.ts
+++ zhuFengTypescript/src/index.ts
@@ -1,3 +1,22 @@
-let name: string = "World"
+class Person {
+  name: string
+  age: number
 
-export {}
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`,
+    )
+  }
+}
+
+function createInstance<T>(
+  target: new (...args: any[]) => T,
+  ...args: ConstructorParameters<typeof target>
+): T {
+  return new target(...args)
+}
